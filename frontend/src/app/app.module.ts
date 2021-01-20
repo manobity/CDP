efcbf92e36af0a57b14d67ad6a23e26f15e006dd
Marco Antonio Gonçalves Junior <marco.junior@nepos.com.br>
2021-01-20 14:09:05 -0300

    Desenvolvimento Home - Registro de localização
    Inserido o registro de localização no modulo principal.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { registerLocaleData } from '@angular/common';
+//registro de localização para pt-br
+//{
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
+//}
+
 import { MenuComponent } from './navegacao/menu/menu.component';
 import { FooterComponent } from './navegacao/footer/footer.component';
 import { HomeComponent } from './navegacao/home/home.component';
